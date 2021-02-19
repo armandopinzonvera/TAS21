c9a4a890aba7a4e91fab9933fbb8894a56f3debf
ArmandoPinzon <armandopinzonvera@gmail.com>
2021-02-18 20:09:03 -0500

    la consulta funciona!!
    es simple, bueno, solo se ingresa el dato de nombre de poroyecto y nos regresa el ID de transecto..
    pero funciona

--- app/src/main/java/com/noFreeGps/tas21/Continuar.java
+++ app/src/main/java/com/noFreeGps/tas21/Continuar.java
@@ -2,12 +2,14 @@ package com.noFreeGps.tas21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.noFreeGps.tas21.SQLite.ConexionSQLite;
 import com.noFreeGps.tas21.SQLite.UtilidadesSQLite;
@@ -25,7 +27,7 @@ public class Continuar extends AppCompatActivity {
 
         conexionSQLite = new ConexionSQLite(getApplicationContext(), UtilidadesSQLite.DDBB_NAME, null, 1 );
 
-        et_c_nombreproyecto = findViewById(R.id.et_nombreProyecto);
+        et_c_nombreproyecto =  findViewById(R.id.et_c_nombreProyecto);
         tv_c_transecto = findViewById(R.id.tv_c_transecto);
         tv_c_sumaTransecto = findViewById(R.id.tv_c_sumaTransecto);
 
@@ -33,11 +35,28 @@ public class Continuar extends AppCompatActivity {
 
     public void buscar(View view) {
         SQLiteDatabase db = conexionSQLite.getReadableDatabase();
-        String´+
+        String [] parametros = {et_c_nombreproyecto.getText().toString()};
+        String[] campos = {UtilidadesSQLite.FK_TRANSECTO};
+
+        try {
+            Cursor cursor =  db.query(UtilidadesSQLite.TABLA_PROYECTO, campos, UtilidadesSQLite.NOMBRE_PROYECTO+"=?", parametros, null, null, null );
+            cursor.moveToFirst();
+            tv_c_transecto.setText(cursor.getString(0));
+
+            cursor.close();
+        } catch (Exception e) {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();;
+            et_c_nombreproyecto.setText("");
+        }
     }
 
     public void continuar(View view) {
 
     }
 
+    public void limpiar(View view) {
+        tv_c_transecto.setText("");
+        tv_c_sumaTransecto.setText("");
+
+    }
 }
