d710a5e822dfc5b5965c20bc1d057b9728390b4b
ArmandoPinzon <armandopinzonvera@gmail.com>
2021-02-21 10:46:23 -0500

    Cara de verga Git, me toco un poco de cosas de nuevo
    GIT: te mentare la madre cada vez que pueda

--- app/src/main/java/com/noFreeGps/tas21/Iniciar.java
+++ app/src/main/java/com/noFreeGps/tas21/Iniciar.java
@@ -1,5 +1,3 @@
-
-
 package com.noFreeGps.tas21;
 //This class just take the project's information
 // and send it to activity VistaTransecto or return
@@ -9,15 +7,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.noFreeGps.tas21.SQLite.ConexionSQLite;
+import com.noFreeGps.tas21.SQLite.UtilidadesSQLite;
 
 public class Iniciar extends AppCompatActivity  {
 
     EditText et_nombreProyecto, et_IdTransecto;
-    Button bt_iniciar, bt_volver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,35 +28,52 @@ public class Iniciar extends AppCompatActivity  {
 
         et_nombreProyecto = findViewById(R.id.et_nombreProyecto);
         et_IdTransecto = findViewById(R.id.et_idTransecto);
-        bt_iniciar = findViewById(R.id.bt_iniciar);
-        bt_volver = findViewById(R.id.bt_volver);
 
-        bt_iniciar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                iniciar();
-            }
-        });
-        bt_volver.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                volver();
-            }
-        });
     }
+
     ///////////////////////////////////
-    //////  Functionality Buttons  ////
+    //////  Functionality Buttons
+    ////    Validacion Text
     ///////////////////////////////////
-    private void volver() {
+    public String validar(){
+
+        String validacion ="bien";
+        String campo1 = et_nombreProyecto.getText().toString();
+        String campo2 = et_IdTransecto.getText().toString();
+        if(campo1.isEmpty() || campo2.isEmpty()){
+            validacion = "vacio";
+        }else if (campo1.equals(campo2)){
+            validacion = "iguales";
+        } else if (campo1.length() > 10 || campo2.length() > 10){
+            validacion = "largo";
+        }
+        return validacion;
+    }
+
+    public void onClickIniciar(View view) {
+        switch (validar()){
+            case "vacio":
+                Toast.makeText(this, "No pueden estar vacios", Toast.LENGTH_LONG).show();
+                break;
+            case "iguales":
+                Toast.makeText(this, "No pueden ser iguales", Toast.LENGTH_LONG).show();
+                et_IdTransecto.setText("");
+                break;
+            case "largo":
+                Toast.makeText(this, "no pueden ser tan grandes", Toast.LENGTH_LONG).show();
+                et_nombreProyecto.setText("");
+                et_IdTransecto.setText("");
+                break;
+            default:   iniciarProyecto();
+        }
+    }
+
+    public void onClickvolver(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
-
     }
-    private void iniciar() {
 
-        startLocationService();
 
-    }
 
     ///////////////////////////////////
     ///// Permiso Localizacion ////////
@@ -72,11 +91,27 @@ public class Iniciar extends AppCompatActivity  {
     ///// pasar a VistaTransecto ////////
     /////////////////////////////////////////////////////////////
 
-    private void startLocationService() {
+    private void iniciarProyecto() {
+
+        ConexionSQLite conexionSQLite = new ConexionSQLite(this, UtilidadesSQLite.DDBB_NAME, null, 1);
+        SQLiteDatabase ddbb = conexionSQLite.getWritableDatabase();
+
+        String insert = "INSERT INTO "+UtilidadesSQLite.TABLA_PROYECTO
+                +" ( " +UtilidadesSQLite.NOMBRE_PROYECTO+", "+UtilidadesSQLite.FK_TRANSECTO+") "
+                +" VALUES ('"+et_nombreProyecto.getText().toString()+"', '"+et_IdTransecto.getText().toString()+"')";
+        ddbb.execSQL(insert);
+        String insert2 = "INSERT INTO "+UtilidadesSQLite.TABLA_TRANSECTO
+                +" ( " +UtilidadesSQLite.ID_TRANSECTO+", "+UtilidadesSQLite.FK_TRACK+") "
+                +" VALUES ('"+et_IdTransecto.getText().toString()+"', '"+0+"')";
+
+        ddbb.execSQL(insert2);
+        et_nombreProyecto.setText("");
+        et_IdTransecto.setText("");
 
         Intent intent = new Intent(getApplicationContext(), VistaTransecto.class);
         startActivity(intent);
     }
+
 
 
 }
