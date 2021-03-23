0c9fe07230d3992453bf1485cae02bf018dfaac5
ArmandoPinzon <armandopinzonvera@gmail.com>
2021-03-22 21:48:40 -0500

    con interconexion con activity continuar y vistaTransecto

--- app/src/main/java/com/noFreeGps/tas21/ui/ContinuarIniciar.java
+++ app/src/main/java/com/noFreeGps/tas21/ui/ContinuarIniciar.java
@@ -2,12 +2,24 @@ package com.noFreeGps.tas21.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.noFreeGps.tas21.MainActivity;
 import com.noFreeGps.tas21.R;
+import com.noFreeGps.tas21.SQLite.ConexionSQLite;
+import com.noFreeGps.tas21.SQLite.UtilidadesSQLite;
+import com.noFreeGps.tas21.SQLite.entidades.Entidad_Tproyecto;
+import com.noFreeGps.tas21.SQLite.entidades.Entidad_Ttrack;
+
+import java.util.ArrayList;
 
 public class ContinuarIniciar extends AppCompatActivity {
 
@@ -27,8 +39,114 @@ public class ContinuarIniciar extends AppCompatActivity {
     }
 
     public void onClickContinuarIniciar(View view) {
+        verificarNuevoTrack(et_ci_idTransecto.getText().toString().trim());
+
+        switch (validar()){
+            case "vacio":
+                Toast.makeText(this, "No pueden estar vacios", Toast.LENGTH_LONG).show();
+                break;
+            case "iguales":
+                Toast.makeText(this, "No pueden ser iguales", Toast.LENGTH_LONG).show();
+                et_ci_idTransecto.setText("");
+                break;
+            case "largo":
+                Toast.makeText(this, "no pueden ser tan grandes", Toast.LENGTH_LONG).show();
+                et_ci_idTransecto.setText("");
+                break;
+            case "existe":
+                Toast.makeText(this, "proyecto ya existe", Toast.LENGTH_LONG).show();
+                et_ci_idTransecto.setText("");
+                break;
+            default:   iniciarProyecto();
+        }
+
     }
 
     public void onClickContinuarvolver(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
+                                         //////////////////////////////////////
+    ///////////////////////////////     //     Validation EditText
+                                        ///////////////////////////////////////
+    public String validar(){
+
+        String validacion ="bien";
+        String campo1 = et_ci_nombreProyecto.getText().toString().trim();
+        String campo2 = et_ci_idTransecto.getText().toString().trim();
+        if(campo1.isEmpty() || campo2.isEmpty()){
+            validacion = "vacio";
+        }else if (campo1.equals(campo2)){
+            validacion = "iguales";
+        } else if (campo1.length() > 10 || campo2.length() > 10){
+            validacion = "largo";
+        } else if(verificarNuevoTrack(et_ci_idTransecto.getText().toString().trim()).equals("existe")) {
+            validacion = "existe";
+        }
+        return validacion;
+    }
+    //************************************   Verficar Proyecto no Exista en la BBDD
+    ArrayList<Entidad_Ttrack> arrayIdTrack;
+
+    private String verificarNuevoTrack(String idTrack) {
+
+        ConexionSQLite conexion = new ConexionSQLite(this);
+        SQLiteDatabase db = conexion.getReadableDatabase();
+        arrayIdTrack = new ArrayList<Entidad_Ttrack>();
+
+        Entidad_Ttrack entidadTproyecto = null;
+
+        String queryProyecto = "SELECT "+ UtilidadesSQLite.ID_TRACK+
+                " FROM "+UtilidadesSQLite.TABLA_TRACK+
+                " WHERE "+UtilidadesSQLite.ID_TRACK+
+                " = '"+idTrack+"';";
+
+        Cursor cursor = db.rawQuery(queryProyecto, null);
+        int columIndex = cursor.getColumnIndex(UtilidadesSQLite.ID_TRACK);
+
+        if(cursor.moveToNext()){
+            Toast toast = Toast.makeText(getApplicationContext(), "   EXISTE   ", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0,0);
+            toast.show();
+            return "existe";
+        }
+        return "";
+
+    }
+                                            //////////////////////////////////////
+    ///////////////////////////////       //     - Send data and begin a new project
+                                          //     - Begin Location - Service
+                                          //     - Continue to next activity: VistaTransecto.java
+                                             ///////////////////////////////////////
+
+    private void iniciarProyecto() {
+        Entidad_Tproyecto entidadTproyecto;
+        Entidad_Ttrack entidadTtrack;
+
+        try {
+            entidadTtrack = new Entidad_Ttrack( et_ci_idTransecto.getText().toString(),"fecha", "hora", 1.111f, 2.222f,2222, et_ci_nombreProyecto.getText().toString());
+            entidadTproyecto = new Entidad_Tproyecto(et_ci_nombreProyecto.getText().toString());
+
+        } catch (Exception e) {
+            entidadTproyecto = new Entidad_Tproyecto("error");
+            entidadTtrack = new Entidad_Ttrack("error","error", "error", 1.111f, 2.222f,1, " error");
+        }
+        ConexionSQLite conexionSQLite = new ConexionSQLite(this);
+
+        boolean success1 = conexionSQLite.addDatoTproyecto(entidadTproyecto);
+        boolean success3 = conexionSQLite.addDatoTtrack(entidadTtrack);
+
+        Toast.makeText(ContinuarIniciar.this, "Exito: "+success1+ ", "+success3, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(getApplicationContext(), VistaTransecto.class);
+        intent.putExtra("extra_1", et_ci_nombreProyecto.getText().toString());
+        intent.putExtra("extra_2", et_ci_idTransecto.getText().toString());
+
+        //showMessage("Nuevo proyecto creado: ", et_nombreProyecto.getText().toString());
+
+        et_ci_idTransecto.setText("");
+        startActivity(intent);
+    }
+
+
 }
